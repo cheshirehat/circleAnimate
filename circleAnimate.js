39a40b0bad1b19ec3acc65deb551ec179575c892
cheshirehat <cheshireinhatter@gmail.com>
2018-09-08 23:31:48 +0900

    update

--- circleAnimate.js
+++ circleAnimate.js
@@ -7,76 +7,72 @@
   const width = canvas.width
   const height = canvas.height
   const endAngle = Math.PI * 2
-  const antiClockWise = false
+  const antiCloclWise = false
 
-	function circleAnimate () {
+  function circleAnimate () {
     circle.clearRect(0, 0, width, height)
     circleBox.forEach(circle => {
       const circleList = circle
-			circleList.updateCircle()
+      circleList.updateCircle()
     })
-		requestAnimationFrame(circleAnimate)
+    requestAnimationFrame(circleAnimate)
   }
-  
-	function circleDraw () {
-		for(let i = 0; i < 50; i++) {
-			circleBox[i] = circleCreate()
-		}
+
+  function circleDraw () {
+    for (let i = 0; i < 50; i++) {
+      circleBox[i] = circleCreate()
+    }
   }
-  
-	function circleCreate () {
-		let xCord = Math.floor(Math.random() * width)
-		let yCord = Math.floor(Math.random() * height)
-		let radius = Math.floor(Math.random() * 100)
-		let updateCircle = function() {
-			circle.beginPath()
-			circle.arc(xCord, yCord, radius, endAngle, antiClockWise)
-			circle.strokeStyle = colorCreateHSL()
-			circle.stroke()
-			if(radius < 100) {
-				radius += 2
-			} else {
-				xCord = Math.floor(Math.random() * width)
-				yCord = Math.floor(Math.random() * height)
-				radius = 0
-			}
-		}
-		return {
+
+  function circleCreate () {
+    let xCord = Math.floor(Math.random() * width)
+    let yCord = Math.floor(Math.random * height)
+    let radius = Math.floor(Math.random() * 100)
+    let updateCircle = () => {
+      circle.beginPath()
+      circle.arc(xCord, yCord, radius, endAngle, antiCloclWise)
+      circle.strokeStyle = colorCreateHSL()
+      circle.stroke()
+      if (radius < 100) {
+        radius += 2
+      } else {
+        xCord = Math.floor(Math.random() * width)
+        yCord = Math.floor(Math.random() * height)
+        radius = 0
+      }
+    }
+    return {
       updateCircle,
       xCord,
       yCord,
       radius
-		}
+    }
   }
-  
+
   // RGBでカラーコード生成
   function colorCreateRGB () {
-    let color
-    const r = Math.round(Math.random() * 255)
-    const g = Math.round(Math.random() * 255)
-    const b = Math.round(Math.random() * 255)
-
-    color = `rgb(${r}, ${g}, ${b})`
+    const r = Math.round(Math.random() *255)
+    const g = Math.round(Math.random() *255)
+    const b = Math.round(Math.random() *255)
+    const color = `rgb(${r}, ${g}, ${b})`
 
     return color
   }
 
   // HSLでカラーコード生成
   function colorCreateHSL () {
-    let color
     const h = Math.random() * 360
-
-    color = `hsl(${h}, 80%, 60%)`
+    const color = `hsl(${h}, 80%, 60%)`
 
     return color
   }
 
-	window.requestAnimationFrame = (() => {
-		return window.requestAnimationFrame ||
-		window.webkitRequestAnimationFrame ||
-		(callback => window.setTimeout(callback, 50))
+  window.requestAnimationFrame = (() => {
+    return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    (callback => window.setTimeout(callback, 50))
   }) ()
-  
-	circleDraw()
-	circleAnimate()
+
+  circleDraw()
+  circleAnimate()
 }) ()
